16e3eb7bb007945c0bf7457ac82bfde2db5788f7
Louis Lefevre <louislefev@gmail.com>
2021-07-20 04:30:24 +0100

    Add start, stop, and restart methods
    Modify pythonProcess to be nullable.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,42 @@
-import { ChildProcess } from 'child_process';
+import { spawn, ChildProcess } from 'child_process';
 import { platform } from 'os';
 
 const defaultPythonPath = platform() === 'win32' ? 'python3' : 'python';
 
 export class PythonInteractive {
   readonly pythonPath: string;
-  pythonProcess: ChildProcess;
+  pythonProcess: ChildProcess | null;
   script: string;
 
   constructor(pythonPath?: string) {
     this.pythonPath = pythonPath ?? defaultPythonPath;
-    this.pythonProcess = <ChildProcess>{};
+    this.pythonProcess = null;
     this.script = '';
+  }
+
+  start() {
+    if (!this.pythonProcess) {
+      this.pythonProcess = spawn(this.pythonPath, ['-i', '-u']);
+      if (this.pythonProcess.stdout && this.pythonProcess.stderr) {
+        this.pythonProcess.stdout.setEncoding('utf8');
+        this.pythonProcess.stderr.setEncoding('utf8');
+      }
+      this.script = '';
+    }
+  }
+
+  stop() {
+    if (this.pythonProcess) {
+      this.pythonProcess.kill();
+      if (this.pythonProcess.stdin) {
+        this.pythonProcess.stdin.end();
+      }
+      this.pythonProcess = null;
+    }
+  }
+
+  restart() {
+    this.stop();
+    this.start();
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -16,20 +16,78 @@ describe('Initialise PythonInteractive', () => {
     python = new PythonInteractive();
   });
 
-  test('PythonPath_IsDefault_True', () => {
+  test('PythonPath_IsDefault_EqualDefaultPath', () => {
     expect(python.pythonPath).toBe('python' || 'python3');
   });
 
-  test('PythonPath_IsDefault_False', () => {
+  test('PythonPath_IsNotDefault_EqualSetPath', () => {
     python = new PythonInteractive('path/to/python');
     expect(python.pythonPath).toBe('path/to/python');
   });
 
   test('PythonProcess_StartsEmpty_ReturnEmptyChildProcess', () => {
-    expect(python.pythonProcess).toMatchObject({});
+    expect(python.pythonProcess).toBe(null);
   });
 
   test('Script_StartsEmpty_ReturnEmptyString', () => {
     expect(python.script).toBe('');
   });
 })
+
+describe('Start and stop Python process', () => {
+  let python: PythonInteractive; 
+  beforeEach(() => {
+    python = new PythonInteractive();
+    python.start();
+    python.script = 'text';
+  });
+  afterEach(() => {
+    python.stop();
+  });
+
+  test('Start_AliveProcess_DoNothing', () => {
+    const process = python.pythonProcess;
+    const script = python.script;
+    python.start();
+    expect(python.pythonProcess).toBe(process);
+    expect(python.script).toBe(script);
+  });
+
+  test('Start_KilledProcess_SpawnProcess', () => {
+    python.stop();
+    python.start();
+    expect(python.pythonProcess).not.toBe(null);
+    expect(python.script).toBe('');
+  });
+
+  test('Stop_KilledProcess_DoNothing', () => {
+    python.stop();
+    const process = python.pythonProcess;
+    const script = python.script;
+    python.stop();
+    expect(python.pythonProcess).toBe(process);
+    expect(python.script).toBe(script);
+  });
+
+  test('Stop_AliveProcess_KillProcess', () => {
+    const script = python.script;
+    python.stop();
+    expect(python.pythonProcess).toBe(null);
+    expect(python.script).toBe(script);
+  });
+
+  test('Restart_KilledProcess_SpawnProcess', () => {
+    python.stop();
+    python.restart();
+    expect(python.pythonProcess).not.toBe(null);
+    expect(python.script).toBe('');
+  });
+
+  test('Restart_AliveProcess_KillThenSpawnProcess', () => {
+    const process = python.pythonProcess;
+    const script = python.script;
+    python.restart();
+    expect(python.pythonProcess).not.toBe(process);
+    expect(python.script).not.toBe(script);
+  });
+});
